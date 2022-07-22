048c36d27277bcac3063c557e432b6519d949b4f
paulfitzpatrick85 <fitzer99@gmail.com>
2022-07-22 18:44:53 +0000

    migration error, in progress of fixing

--- genre/admin.py
+++ genre/admin.py
@@ -1,15 +1,16 @@
+
 from django.contrib import admin
-from .models import Genre 
+from .models import Genre
 from django_summernote.admin import SummernoteModelAdmin
 
+admin.site.register(Genre)
+# @admin.register(Genre)
+# class GenreAdmin(SummernoteModelAdmin):
 
-@admin.register(Genre)
-class GenreAdmin(SummernoteModelAdmin):
+#     # prepopulated_fields = {'slug': ('genre_name',)}  # populate slug(form?) from title
+#     # list_filter = ('status', 'created_on')  # will create filter window/box with status and created today, last 7 days etc
+#     # list_display = ('genre_name', 'slug', 'status', 'created_on')  # titles/info on each post, slug removed
+#     # search_fields = ('genre_name', 'content')  # add search bar in admin page
 
-    prepopulated_fields = {'slug': ('genre_name',)}  # populate slug(form?) from title
-    list_filter = ('status', 'created_on')  # will create filter window/box with status and created today, last 7 days etc
-    list_display = ('genre_name', 'slug', 'status', 'created_on')  # titles/info on each post, slug removed
-    search_fields = ('genre_name', 'content')  # add search bar in admin page
-
-    summernote_fields = ('content')
+#     summernote_fields = ('content')
 
--- genre/models.py
+++ genre/models.py
@@ -8,27 +8,30 @@ STATUS = ((0, 'Draft'), (1, 'Published'))   # -may not use
 
 # genre model
 class Genre(models.Model):
-    genre_name = models.CharField(max_length=200, unique=True, default='DEFAULT VALUE')
+    genre_name = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='genre')  # if user is deleted, their posts are too
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='genre')  # if user is deleted, their posts are to
+    updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     genre_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
-    updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)    
     
+    class Meta:
+        ordering = ['-created_on']
+
     def __str__(self):
         return self.genre_name
 
+
 class Band(models.Model):
-    genre = models.ForeignKey(Genre, on_delete=models.CASCADE, related_name='band')
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE, related_name='bands')
     # slug = models.SlugField(max_length=100, unique=True)  # taken from post/genre class
     band_name = models.CharField(max_length=80)
     band_email = models.EmailField()
     bio_body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True) 
-    updated_on = models.DateTimeField(auto_now=True)
     Band_approved = models.BooleanField(default=False)   
     # status = models.IntegerField(choices=STATUS, default=0)
     # created_on = models.DateTimeField(auto_now_add=True)
@@ -39,3 +42,4 @@ class Band(models.Model):
 
     def __str__(self):
         return f"Band {self.bio_body} by {self.band_name}"  # add image when code working properly
+

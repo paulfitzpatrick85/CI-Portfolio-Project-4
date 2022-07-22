3db0f42ac3d775fe8bcf32a08216574ce0c6a60d
paulfitzpatrick85 <fitzer99@gmail.com>
2022-07-22 16:58:04 +0000

    add genre model(throwing error)

--- genre/models.py
+++ genre/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+# Weather post is draft or published/live
+STATUS = ((0, 'Draft'), (1, 'Published'))   # -may not use
+
+
+# genre model
+class Genre(models.Model):
+    genre_name = models.CharField(max_length=200, unique=True, default='DEFAULT VALUE')
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='genre')  # if user is deleted, their posts are too
+    content = models.TextField()
+    genre_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)    
+    
+    def __str__(self):
+        return self.genre_name
+
+class Band(models.Model):
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE, related_name='band')
+    # slug = models.SlugField(max_length=100, unique=True)  # taken from post/genre class
+    band_name = models.CharField(max_length=80)
+    band_email = models.EmailField()
+    bio_body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True) 
+    updated_on = models.DateTimeField(auto_now=True)
+    Band_approved = models.BooleanField(default=False)   
+    # status = models.IntegerField(choices=STATUS, default=0)
+    # created_on = models.DateTimeField(auto_now_add=True)
+    # image = models.ImageField(upload_to='images')
+
+    class Meta:
+        ordering = ['created_on']  # order post by create_on, from above, '-' means use descending order
+
+    def __str__(self):
+        return f"Band {self.bio_body} by {self.band_name}"  # add image when code working properly

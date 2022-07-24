30d8abcdc89d7cf6b9a44b4124c11dc68bea78f8
paulfitzpatrick85 <fitzer99@gmail.com>
2022-07-24 12:59:47 +0000

    add genrelist view error

--- genres/views.py
+++ genres/views.py
@@ -5,6 +5,6 @@ from .models import Genre
 
 class GenreList(generic.ListView):
     model = Genre
-    queryset = Genre.objects.filter(status=1).order_by('-created_on')  # change back to 'word' to order alphabetically
+    # queryset = Genre.objects.filter(status=1).order_by('word')  # change back to 'word' to order alphabetically
     template_name = 'index.html'     # html file that view will render
     paginate_by = 4        # limit number of post to 6, if there are more django will add page navagation

690c95bb0181960b93fcd253624b5fd5160ee47c
paulfitzpatrick85 <fitzer99@gmail.com>
2022-07-25 18:31:26 +0000

    change display of genres

--- genres/views.py
+++ genres/views.py
@@ -7,4 +7,4 @@ class GenreList(generic.ListView):
     model = Genre
     # queryset = Genre.objects.filter(status=1).order_by('word')  # change back to 'word' to order alphabetically
     template_name = 'index.html'     # html file that view will render
-    paginate_by = 4        # limit number of post to 6, if there are more django will add page navagation
+    paginate_by = 6       # limit number of post to 6, if there are more django will add page navagation

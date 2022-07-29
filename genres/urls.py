39dba8465a8eda2b021ee0848c857959be2ce5c4
paulfitzpatrick85 <fitzer99@gmail.com>
2022-07-29 20:02:16 +0000

    wire up genre title and image to genre_detail.html

--- genres/urls.py
+++ genres/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.GenreList.as_view(), name='home'),   
+    path('', views.GenreList.as_view(), name='home'),
+    path('<slug:slug>/', views.GenreDetail.as_view(), name='genre_detail'), 
 ]
--- genres/views.py
+++ genres/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Genre
 
 
@@ -7,3 +7,20 @@ class GenreList(generic.ListView):
     model = Genre
     template_name = 'index.html'   
     paginate_by = 6
+
+
+class GenreDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):    
+        queryset = Genre.objects.filter(status=1)   
+        genre = get_object_or_404(queryset, slug=slug)  
+        bands = genre.bands.filter(Band_approved=True)
+
+        return render(
+            request,            
+            "genre_detail.html",       
+            {
+                "genre": genre,          # render dictionary
+                "bands": bands,
+            },
+        )

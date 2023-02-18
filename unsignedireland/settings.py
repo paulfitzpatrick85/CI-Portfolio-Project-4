5dc687588fa0df126b165592fa02073371275c17
paulfitzpatrick85 <fitzer99@gmail.com>
2023-02-18 23:42:35 +0000

    add cloudinary again2

--- unsignedireland/settings.py
+++ unsignedireland/settings.py
@@ -154,7 +154,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+# STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
